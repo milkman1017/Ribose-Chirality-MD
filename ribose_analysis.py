97bda8191aea682d644423aff11cbebb73919203
milkman1017 <milkmanmahler1017@gmail.com>
2023-07-16 21:52:21 -0400

    bug fixes

--- ribose_analysis.py
+++ ribose_analysis.py
@@ -72,87 +72,6 @@ def compute_hbonds(chunk, hbond_data, sim_number, config):
 
     return hbond_data
 
-def hbond_heatmap(hbond_counts):
-
-    dribose_donor_labels = set()
-    dribose_acceptor_labels = set()
-
-    lribose_donor_labels = set()
-    lribose_acceptor_labels = set()
-
-    hbonds = hbond_counts
-
-    for residue, bond_dict in hbonds.items():
-        if residue == 'DRI-G' or residue == 'DRI-C' or residue == 'G-DRI' or residue == 'C-DRI':
-            donor_residue, acceptor_residue = residue.split('-')
-            for atom in bond_dict.keys():
-                dribose_donor_labels.add(f"{donor_residue}-{atom.split('-')[0]}")
-                dribose_acceptor_labels.add(f"{acceptor_residue}-{atom.split('-')[1]}")
-
-        elif residue == 'LRI-C' or residue == 'LRI-G' or residue == 'G-LRI' or residue == 'C-LRI':
-            donor_residue, acceptor_residue = residue.split('-')
-            for atom in bond_dict.keys():
-                lribose_donor_labels.add(f"{donor_residue}-{atom.split('-')[0]}")
-                lribose_acceptor_labels.add(f"{acceptor_residue}-{atom.split('-')[1]}")
-
-    dribose_donor_labels = sorted(dribose_donor_labels, key=ribose_label_sort)
-    dribose_acceptor_labels = sorted(dribose_acceptor_labels, key=ribose_label_sort,reverse=True)
-
-    lribose_donor_labels = sorted(lribose_donor_labels,key=ribose_label_sort)
-    lribose_acceptor_labels = sorted(lribose_acceptor_labels, key=ribose_label_sort,reverse=True)
-    
-    dribose_bond_data = np.zeros((len(dribose_donor_labels), len(dribose_acceptor_labels)))
-    lribose_bond_data = np.zeros((len(lribose_donor_labels), len(lribose_acceptor_labels)))
-    
-    for residue, bond_dict in hbonds.items():
-        if residue == 'DRI-G' or residue == 'DRI-C' or residue == 'G-DRI' or residue == 'C-DRI':
-            donor_residue, acceptor_residue = residue.split('-')
-
-            for atom, count in bond_dict.items():
-                donor, acceptor = atom.split('-')
-                donor_label = f"{donor_residue}-{donor}"
-                acceptor_label = f"{acceptor_residue}-{acceptor}"
-                donor_index = dribose_donor_labels.index(donor_label)
-                acceptor_index = dribose_acceptor_labels.index(acceptor_label)
-                dribose_bond_data[donor_index, acceptor_index] += count
-
-        elif residue == 'LRI-C' or residue == 'LRI-G' or residue == 'G-LRI' or residue == 'C-LRI':
-            donor_residue, acceptor_residue = residue.split('-')
-            for atom, count in bond_dict.items():
-                donor, acceptor = atom.split('-')
-                donor_label = f"{donor_residue}-{donor}"
-                acceptor_label = f"{acceptor_residue}-{acceptor}"
-                donor_index = lribose_donor_labels.index(donor_label)
-                acceptor_index = lribose_acceptor_labels.index(acceptor_label)
-                lribose_bond_data[donor_index, acceptor_index] += count
-
-    fig, ax = plt.subplots(1,2)
-    im1 = ax[0].imshow(dribose_bond_data, cmap="hot")
-
-    ax[0].set_xticks(np.arange(len(dribose_acceptor_labels)))
-    ax[0].set_xlabel('Acceptors')
-    ax[0].set_yticks(np.arange(len(dribose_donor_labels)))
-    ax[0].set_xticklabels(dribose_acceptor_labels)
-    ax[0].set_yticklabels(dribose_donor_labels)
-    ax[0].set_ylabel('Donors')
-    ax[0].set_title('D-Ribose Contact Map')
-
-    im2 = ax[1].imshow(lribose_bond_data, cmap='hot')
-    ax[1].set_xticks(np.arange(len(lribose_acceptor_labels)))
-    ax[1].set_xlabel('Acceptors')
-    ax[1].set_yticks(np.arange(len(lribose_donor_labels)))
-    ax[1].set_xticklabels(lribose_acceptor_labels)
-    ax[1].set_yticklabels(lribose_donor_labels)
-    ax[1].set_ylabel('Donors')
-    ax[1].set_title('L-Ribose Contact Map')
-
-    plt.setp(ax[0].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    plt.setp(ax[1].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-    
-    plt.tight_layout()
-    plt.suptitle('Hydrogen Bond Heat Map')
-    plt.show()
-
 def hbond_counts_distribution(hbonds, config):
     test_mols = config.get('Input Setup','test resnames').split(',')
     sheet_mols = config.get('Input Setup','crystal resnames').split(',')
@@ -213,6 +132,8 @@ def graph_nematic_order(nematic_order, test_resnames):
     plt.ylabel('Probability Density')
     plt.legend()
     plt.show()
+
+
         
 def compute_sasa(traj, test_res_names, SASA):
     sasa = md.shrake_rupley(traj, mode='residue')
@@ -245,68 +166,68 @@ def graph_sasa(SASA, test_resnames):
     plt.legend()
     plt.show()
 
+
 def main():
     config = get_config()
-    sims = int(config.get('Analyses','number of sims'))
-    nsteps = int(config.get('Input Setup','number steps'))
-    cell_name = config.get('Input Setup','crystal structure')
-    sheet_resnames = config.get('Input Setup','crystal resnames').split(',')
-    test_resnames = config.get('Input Setup','test resnames').split(',')
-    report = int(config.get('Input Setup','report interval'))
+    sims = int(config.get('Analyses', 'number of sims'))
+    nsteps = int(config.get('Input Setup', 'number steps'))
+    cell_name = config.get('Input Setup', 'crystal structure')
+    sheet_resnames = config.get('Input Setup', 'crystal resnames').split(',')
+    test_resnames = config.get('Input Setup', 'test resnames').split(',')
+    report = int(config.get('Input Setup', 'report interval'))
     total_mols = test_resnames + sheet_resnames
 
-    file_path = config.get('Input Setup','file path')
-    outdir = config.get('Output Parameters','output directory')
+    file_path = config.get('Input Setup', 'file path')
+    outdir = config.get('Output Parameters', 'output directory')
 
-    #these need to be initialized as lists of lists since list lengths are dynamic
-    #if the number of each mols are different then an array cant be used as well since the columns would be of different legnths
+    # these need to be initialized as lists of lists since list lengths are dynamic
+    # if the number of each mols are different then an array can't be used as well since the columns would be of different lengths
     heights = [[] for _ in range(len(test_resnames))]
     SASA = [[] for _ in range(len(test_resnames))]
 
-    #initalize hbonds as pandas dataframe
+    # initialize hbonds as pandas dataframe
     hbond_columns = pd.MultiIndex.from_product([range(sims), test_resnames, total_mols],
-                                         names=['sim_number', 'mol_1', 'mol_2'])
+                                               names=['sim_number', 'mol_1', 'mol_2'])
     hbonds = pd.DataFrame(columns=hbond_columns)
 
-    #these can be initalized as arrays as each mol type will give only one output so the size of the array is easily known prior to calculating
-    #these give outputs of length one no matter the number of mols so an array works
-    nematic_order = np.zeros((int(nsteps/report), len(test_resnames), sims))
+    # these can be initialized as arrays as each mol type will give only one output so the size of the array is easily known prior to calculating
+    # these give outputs of length one no matter the number of mols so an array works
+    nematic_order = np.zeros((int(nsteps / report), len(test_resnames), sims))
+
+    contact_maps = np.zeros((sims, len(test_resnames), len(test_resnames)))
 
     for sim_number in range(sims):
         print('Analyzing sim number', sim_number)
 
-        traj = md.iterload(f'{file_path}/traj_{sim_number}_sheet_{cell_name[:-4]}_mols_{"_".join(test_resnames)}_steps_{nsteps}.dcd', 
-                            top=f'{file_path}/traj_{sim_number}_sheet_{cell_name[:-4]}_mols_{"_".join(test_resnames)}_steps_{nsteps}.pdb')
+        traj = md.iterload(f'{file_path}/traj_{sim_number}_sheet_{cell_name[:-4]}_mols_{"_".join(test_resnames)}_steps_{nsteps}.dcd',
+                           top=f'{file_path}/traj_{sim_number}_sheet_{cell_name[:-4]}_mols_{"_".join(test_resnames)}_steps_{nsteps}.pdb')
 
         for chunk in traj:
 
-            if config.get('Analyses','height density') == 'True':
+            if config.get('Analyses', 'height density') == 'True':
                 for i, mol in enumerate(test_resnames):
                     heights[i].extend(compute_heights(chunk, sheet_resnames, mol))
 
-            if config.get('Analyses','Nematic Order Parameter') == 'True':
+            if config.get('Analyses', 'Nematic Order Parameter') == 'True':
                 nematic_order = compute_nematic_order(chunk, nematic_order, test_resnames, sim_number)
 
-            if config.get('Analyses','Solvent Accessible Surface Area') == 'True':
+            if config.get('Analyses', 'Solvent Accessible Surface Area') == 'True':
                 SASA = compute_sasa(chunk, test_resnames, SASA)
 
-            if config.get('Analyses','H-Bond Counts')=='True':
+            if config.get('Analyses', 'H-Bond Counts') == 'True':
                 hbonds = compute_hbonds(chunk, hbonds, sim_number, config)
-
-    # hbond_heatmap(hbond_counts)
-
-    if config.get('Analyses','height density') == 'True':
+                
+    if config.get('Analyses', 'height density') == 'True':
         graph_heights(heights, config)
 
-    if config.get('Analyses','Nematic Order Parameter') == 'True':
+    if config.get('Analyses', 'Nematic Order Parameter') == 'True':
         graph_nematic_order(nematic_order, test_resnames)
 
-    if config.get('Analyses','Solvent Accessible Surface Area') == 'True':
+    if config.get('Analyses', 'Solvent Accessible Surface Area') == 'True':
         graph_sasa(SASA, test_resnames)
-    
-    if config.get('Analyses','H-Bond Counts') == 'True':
-        hbond_counts_distribution(hbonds, config)
 
+    if config.get('Analyses', 'H-Bond Counts') == 'True':
+        hbond_counts_distribution(hbonds, config)
 
 
 if __name__ == '__main__':
